592689323b54e60b3c7ea45fc1610094e1b794fe
Nikos Grigoriou <iis21084@uom.edu.gr>
2024-01-28 14:21:59 +0200

    Add Get method to fetch all products from the API

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,76 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AsyncPipe, JsonPipe } from '@angular/common';
+
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../interfaces/product';
+import { Observable, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, JsonPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
 export class HomeComponent implements OnInit  {
 
-  productList!:Product[];
+  products$!:Observable<Product[]>;
+  productList: Product[] = [];
 
-  constructor(private productService:ProductService){}
+  constructor(private productService: ProductService) {
+    this.products$ = this.productService.product$.pipe(
+      // Use tap to update productList
+      tap(products => {
+        this.productList = products;
+        console.log(products)
+      })
+    );
+  }
 
   ngOnInit(): void {
-    this.productList = [
-      {
-        id: 1,
-        title: "iPhone 15",
-        description: "An apple mobile which is nothing like apple",
-        price: 1.049,
-        discountPercentage: 12.96,
-        rating: 4.69,
-        stock: 94,
-        brand: "Apple",
-        category: "smartphones",
-        thumbnail: "https://cdn.mobilesyrup.com/wp-content/uploads/2022/11/iphone-15-pro-black-scaled.jpg",
-        images: ["path/to/image1_1.jpg", "path/to/image1_2.jpg", "path/to/image1_3.jpg"],
-      },
-      {
-        id: 2,
-        title: "Samsung Galaxy S21",
-        description: "A powerful Android smartphone from Samsung",
-        price: 699,
-        discountPercentage: 8.5,
-        rating: 4.75,
-        stock: 120,
-        brand: "Samsung",
-        category: "smartphones",
-        thumbnail: "path/to/thumbnail2.jpg",
-        images: ["path/to/image2_1.jpg", "path/to/image2_2.jpg", "path/to/image2_3.jpg"],
-      },
-      {
-        id: 3,
-        title: "MacBook Pro",
-        description: "A high-performance laptop from Apple",
-        price: 1299,
-        discountPercentage: 10,
-        rating: 4.85,
-        stock: 50,
-        brand: "Apple",
-        category: "laptops",
-        thumbnail: "path/to/thumbnail3.jpg",
-        images: ["path/to/image3_1.jpg", "path/to/image3_2.jpg", "path/to/image3_3.jpg"],
-      },
-      {
-        id: 4,
-        title: "Sony WH-1000XM4",
-        description: "Premium noise-canceling headphones from Sony",
-        price: 349,
-        discountPercentage: 15,
-        rating: 4.9,
-        stock: 75,
-        brand: "Sony",
-        category: "audio",
-        thumbnail: "path/to/thumbnail4.jpg",
-        images: ["path/to/image4_1.jpg", "path/to/image4_2.jpg", "path/to/image4_3.jpg"],
-      },
-    ];//get data from the service
+    this.productService.getAllProducts();
   }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,12 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { BehaviorSubject } from 'rxjs';
+import { Product } from '../interfaces/product';
+
+import { tap } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private url = 'https://dummyjson.com';
+
+    //Subject to hold product list
+    private _productsSub = new BehaviorSubject<Product[]>([])
+    public readonly product$ = this._productsSub.asObservable()
+
+  constructor(private http:HttpClient) {}
+
+  getAllProducts(): void {
+
+    // Make the HTTP GET request with options
+    this.http.get<any>(`${this.url}/products`).subscribe(data => {
+      this._productsSub.next(data?.products);
+      console.log(data)
+    });
+  }
+
 
 
 }
